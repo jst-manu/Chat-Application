a91209bd1ab262cf5e4eec1635b4bfd450fdf2e5
jst-manu <manojs666manu@gmail.com>
2024-06-02 01:59:01 +0530

    done

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ let socketsConected = new Set()
 io.on('connection', onConnected)
 
 function onConnected(socket) {
-  console.log('Socket connected', socket.id)
+  //console.log('Socket connected', socket.id)
   socketsConected.add(socket.id)
   io.emit('clients-total', socketsConected.size)
 
   socket.on('disconnect', () => {
-    console.log('Socket disconnected', socket.id)
+    //console.log('Socket disconnected', socket.id)
     socketsConected.delete(socket.id)
     io.emit('clients-total', socketsConected.size)
   })
